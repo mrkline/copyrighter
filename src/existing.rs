e07e4031ffad066e00dda986752a725bb9aaaada
Matt Kline <matt@bitbashing.io>
2016-08-15 17:21:29 -0700

    Use mpsc instead of messing with CondVars
    Also do much more graceful I/O error handling.

--- src/existing.rs
+++ src/existing.rs
@@ -3,9 +3,9 @@
 //! to Git.
 
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{self, BufReader};
 use std::io::prelude::*;
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::mpsc;
 use std::thread;
 
 use git_historian::PathSet;
@@ -15,59 +15,51 @@ use regex::Regex;
 
 use common::{Year, YearMap};
 
-struct SharedState {
-    // We'll keep track of how many paths we have left in order to know
-    // when to stop waiting for results.
-    paths_remaining: usize,
-
-    // Option is used so we can extract the result at the end.
-    // See http://stackoverflow.com/q/29177449/713961.
-    result : Option<YearMap>,
-}
-
-struct SyncState {
-    mutex : Mutex<SharedState>,
-    // We'll notify the CV when we have no remaining paths to process.
-    cv : Condvar
-}
-
 #[inline]
 pub fn get_year_map(paths: PathSet) -> thread::JoinHandle<YearMap> {
     thread::spawn(|| get_year_map_thread(paths))
 }
 
 fn get_year_map_thread(paths: PathSet) -> YearMap {
-    // Strap together an ARC for all our shared state
-    let shared_state = Arc::new(
-        SyncState{ mutex: Mutex::new(SharedState{paths_remaining: paths.len(),
-                                                 result: Some(YearMap::new())}),
-                   cv: Condvar::new() });
-
     // Let's paralellize! I'm assuming this process will be largely bottlenecked
     // by the I/O of actually reading the files, but we can let the OS'es I/O
     // scheduler figure that out.
     let thread_pool = ThreadPool::new(num_cpus::get());
 
+    let (tx, rx) = mpsc::channel();
+
     for path in paths.into_iter() { // Consume our paths
-        let ss = shared_state.clone(); // Bump the refcount
-        thread_pool.execute(|| scan_file(path, ss));
+        let tx_clone = tx.clone();
+        thread_pool.execute(move || {
+            let result = scan_file(&path);
+            tx_clone.send((path, result)).unwrap()
+        });
+    }
+    // Dropping the original tx here means rx.recv() will fail
+    // once all senders have finished.
+    drop(tx);
+
+    let mut ret = YearMap::new();
+
+    // Slurp our our paths until there aren't any more
+    while let Ok((path, result)) = rx.recv() {
+        // scan_file succeeded or we should print the I/O error and move on.
+        match result {
+            Ok(v) => assert!(ret.insert(path, v).is_none()),
+            Err(e) => writeln!(&mut io::stderr(), "Error reading {}: {}", path, e).unwrap()
+        };
     }
 
-    // Sleep until we've processed all paths.
-    let mut guard = shared_state.mutex.lock().unwrap();
-    while guard.paths_remaining > 0 {
-        guard = shared_state.cv.wait(guard).unwrap();
-    }
-    guard.result.take().unwrap()
+    ret
 }
 
-fn scan_file(path: String, ss : Arc<SyncState>) {
+fn scan_file(path: &str) -> io::Result<Vec<Year>> {
     // Open the file and read in the first line.
     let mut first_line = String::new();
     {
-        let fh = File::open(&path).expect(&("Error opening ".to_string() + &path));
+        let fh = try!(File::open(path));
         let mut br = BufReader::new(fh);
-        br.read_line(&mut first_line).expect(&("Error reading ".to_string() + &path));
+        try!(br.read_line(&mut first_line));
     }
 
     lazy_static!{
@@ -77,15 +69,12 @@ fn scan_file(path: String, ss : Arc<SyncState>) {
             r"((\d{4})\s*[-–—]\s*(\d{4}))|(\d{4})").unwrap();
     }
 
+    let mut years : Vec<Year> = Vec::new();
+
     // The first line isn't a copyright line. Move on to the next file.
     if !COPYRIGHT.is_match(&first_line) {
-        let mut guard = ss.mutex.lock().unwrap();
-        guard.paths_remaining -= 1;
-        if guard.paths_remaining == 0 { ss.cv.notify_all(); }
-        return;
+        return Ok(years);
     }
-
-    let mut years : Vec<Year> = Vec::new();
 
     for cap in YEAR_OR_RANGE.captures_iter(&first_line) {
         match cap.at(1) {
@@ -103,11 +92,5 @@ fn scan_file(path: String, ss : Arc<SyncState>) {
         };
     }
 
-    // Take the lock on the shared state
-    let mut guard = ss.mutex.lock().unwrap();
-    // Insert our newfound years.
-    guard.result.as_mut().unwrap().insert(path, years);
-
-    guard.paths_remaining -= 1;
-    if guard.paths_remaining == 0 { ss.cv.notify_all(); }
+    Ok(years)
 }
--- src/update.rs
+++ src/update.rs
@@ -1,11 +1,9 @@
 //! Updates copyright headers based on the information gathered.
 
-#![allow(mutex_atomic)] // We need a mutex for the CV, so atomics aren't helpful.
-
 use std::fs::{File, OpenOptions};
 use std::io;
 use std::io::prelude::*;
-use std::sync::{Arc, Condvar, Mutex};
+use std::sync::{Arc, mpsc};
 use std::os::unix::prelude::*;
 use std::ptr;
 
@@ -17,53 +15,48 @@ use threadpool::ThreadPool;
 
 use common::{Year, YearMap};
 
-struct SyncState {
-    // Hold onto the string given to us via command line args
-    organization: String,
-
-    // We'll keep track of how many paths we have left in order to know
-    // when to stop waiting for results.
-    paths_remaining : Mutex<usize>,
-    // We'll notify the CV when we have no remaining paths to process.
-    cv : Condvar,
-}
-
 pub fn update_headers(map: YearMap, organization: String) {
-    // Strap together an ARC for all our shared state
-    let shared_state = Arc::new(
-        SyncState{ organization: organization,
-                   paths_remaining: Mutex::new(map.len()),
-                   cv: Condvar::new() });
-
     // Let's paralellize! I'm assuming this process will be largely bottlenecked
     // by the I/O of actually reading the files, but we can let the OS'es I/O
     // scheduler figure that out.
     let thread_pool = ThreadPool::new(num_cpus::get());
 
-    for (k, v) in map {
-        let ss = shared_state.clone();
-        thread_pool.execute(|| update_file(k, v, ss));
-    }
+    let (tx, rx) = mpsc::channel();
 
-    // Sleep until we've processed all paths.
-    let mut remaining = shared_state.paths_remaining.lock().unwrap();
-    while *remaining > 0 {
-        remaining = shared_state.cv.wait(remaining).unwrap();
+    let organization = Arc::new(organization);
+
+    for (k, v) in map {
+        let tx_clone = tx.clone();
+        let org_clone = organization.clone();
+        thread_pool.execute(move || {
+            let result = update_file(&k, v, &org_clone);
+            tx_clone.send((k, result)).unwrap();
+        });
+    }
+    // Dropping the original tx here means rx.recv() will fail
+    // once all senders have finished.
+    drop(tx);
+
+
+    // Slurp our our paths until there aren't any more
+    while let Ok((path, result)) = rx.recv() {
+        match result {
+            Ok(()) => { /* Everything worked, nothing to do */ },
+            Err(e) => writeln!(&mut io::stderr(), "Error updating {}: {}", path, e).unwrap()
+        }
     }
 }
 
 /// Update the existing copyright notice of a file, or tack on a new one.
-fn update_file(path: String, years : Vec<Year>, ss : Arc<SyncState>) {
+fn update_file(path: &str, years : Vec<Year>, organization: &str) -> io::Result<()> {
     // Open the file with read and write perms.
-    let mut fh = OpenOptions::new().read(true).write(true).open(&path)
-        .expect(&("Error opening ".to_string() + &path));
+    let mut fh = try!(OpenOptions::new().read(true).write(true).open(path));
 
     // Read in the existing first line (so we can look for an existing notice).
     let mut first_line_buff = String::new();
     {
         let mut br = io::BufReader::new(&fh);
-        br.read_line(&mut first_line_buff)
-          .expect(&("Error reading ".to_string() + &path));
+        try!(br.read_line(&mut first_line_buff));
     }
 
     // We don't want to mess with the newline (or trailing space).
@@ -96,32 +89,27 @@ fn update_file(path: String, years : Vec<Year>, ss : Arc<SyncState>) {
     // TODO: Also allow dashed ranges.
     new_first_line.push_str(&years.into_iter().map(|y| y.to_string()).join(","));
     new_first_line.push(' ');
-    new_first_line.push_str(&ss.organization);
+    new_first_line.push_str(organization);
 
     if !replacing_existing_notice {
         // We need a newline if we're creating our own notice.
         new_first_line.push('\n');
         // Slide the existing contents forward, making way for the new notice.
-        slide_file_contents(&fh, 0, new_first_line.len() as isize);
+        try!(slide_file_contents(&fh, 0, new_first_line.len() as isize));
     }
     else {
         // Calculate the difference in length between the old notice and the new
         // one, then slide all contents *after* the old notice that distance.
         let slide_amount = new_first_line.len() as isize - old_first_line.len() as isize;
-        slide_file_contents(&fh, old_first_line.len(), slide_amount);
+        try!(slide_file_contents(&fh, old_first_line.len(), slide_amount));
     }
 
     // Rewind to the start and write our notice line.
-    fh.seek(io::SeekFrom::Start(0))
-      .expect(&("Error seeking in ".to_string() + &path));
+    try!(fh.seek(io::SeekFrom::Start(0)));
 
-    fh.write_all(new_first_line.as_bytes())
-      .expect(&("Error writing to ".to_string() + &path));
+    try!(fh.write_all(new_first_line.as_bytes()));
 
-    // Decrement the number of files to go.
-    let mut remaining = ss.paths_remaining.lock().unwrap();
-    *remaining -= 1;
-    if *remaining == 0 { ss.cv.notify_all(); }
+    Ok(())
 }
 
 /// We slide file contents around using mmap and memmove, assuming
@@ -131,20 +119,20 @@ fn update_file(path: String, years : Vec<Year>, ss : Arc<SyncState>) {
 ///    then overwriting the existing file with the temp file.
 /// 3. The file fits comfortably in memory space. Besides, if a *code* file
 ///    is more than a few dozen kilobytes, you have other problems.
-fn slide_file_contents(rust_handle : &File, offset: usize, amount : isize) {
+fn slide_file_contents(rust_handle : &File, offset: usize, amount : isize) -> io::Result<()> {
     // Don't let us slide contents past the start of the file.
     assert!(offset as isize + amount >= 0);
 
     // If we have nothing to do, go home early.
     if amount == 0 {
-        return;
+        return Ok(());
     }
 
     // Generally, casting a file length to a isize would be a terrible idea.
     // (usize is 32 bits on x86, and files can be much larger than 4GB.)
     // But we're trying to mmap it (so it should fit in our address space),
     // and if a code file is that big...
-    let file_length_64 : u64 = rust_handle.metadata().unwrap().len();
+    let file_length_64 : u64 = try!(rust_handle.metadata()).len();
     if file_length_64 > isize::max_value() as u64 {
         panic!("One of the given code files is > 2GB. Call a doctor...");
     }
@@ -156,7 +144,7 @@ fn slide_file_contents(rust_handle : &File, offset: usize, amount : isize) {
 
     if amount < 0 { // We have to shrink the file.
         // Shift its contents over.
-        let mut mapping = Mapping::open(fd, file_length).unwrap();
+        let mut mapping = try!(Mapping::open(fd, file_length));
         unsafe { // memmove, a la Rust
             ptr::copy(mapping.ptr().offset(offset as isize),
                       mapping.mut_ptr().offset(offset as isize + amount),
@@ -177,7 +165,7 @@ fn slide_file_contents(rust_handle : &File, offset: usize, amount : isize) {
         }
 
         // Shift the contents over.
-        let mut mapping = Mapping::open(fd, new_length as usize).unwrap();
+        let mut mapping = try!(Mapping::open(fd, new_length as usize));
         unsafe { // memmove, a la Rust
             ptr::copy(mapping.ptr().offset(offset as isize),
                       mapping.mut_ptr().offset(offset as isize + amount),
@@ -187,6 +175,7 @@ fn slide_file_contents(rust_handle : &File, offset: usize, amount : isize) {
     else { // wat
         unreachable!("We should account for this case with an early return.");
     }
+    Ok(())
 }
 
 // A whole crate exists for cross-platform memory mapping
